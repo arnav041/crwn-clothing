43bcb0614a7f0a198b97bd0aaa12d55594bb2500
arnav <programmingunderhoodie@gmail.com>
2021-10-13 23:49:03 +0530

    test commit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -41,7 +41,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         }
     }
     return userRef;
-}
+};
 
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef = firestore.collection(collectionKey)
